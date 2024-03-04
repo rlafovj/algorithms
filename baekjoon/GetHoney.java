2f46f7cf6176d53c1ad2b09f299f62b0f6b20b9f
unknown <rlafovj@gmail.com>
2024-03-04 21:01:28 +0900

    240304 GetHoney

--- baekjoon/GetHoney.java
+++ baekjoon/GetHoney.java
@@ -6,47 +6,30 @@ public class GetHoney {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] place = new int[n];
+        int sum = 0, max = 0;
+        int[] arr = new int[n];
         for(int i =0; i < n; i++){
-            place[i] = sc.nextInt();
+            arr[i] = sc.nextInt();
+            sum += arr[i];
         }
-
-        int max1 = 0, a1 = 0;
-        for (int i = 1; i < n; i++){
-            a1 += place[i];
-        }
-        for(int m = 1; m < n; m++){
-            int b1 = 0, b1Max = a1, b1Maxi = 0;
-            for (int i = n-(n-m)+1; i < n; i++){
-                b1 += place[i];
-                if(b1 > b1Max){
-                    b1Max = b1;
-                    b1Maxi = i;
-                    a1 = a1-place[b1Maxi];
-                }
-            }
-            if (max1 < a1 + b1){
-                max1 = a1 + b1;
+        for(int i = 0; i < n; i++){
+            if(arr[i] > max){
+                max = arr[i];
             }
         }
-        System.out.println(max1);
+        //bHB
+        max += sum - arr[0] - arr[n-1];
 
-        int max2 = 0;
-        for(int m = 1; m < n-1; m++){
-            int a = 0, b = 0, aMax = 0, bMax = 0;
-            for(int i = 1; i <= m; i++){
-                a += place[i];
-            }
-            for(int i = m; i < n-1; i++){
-                b += place[i];
-            }
-            if(max2 < a + b){
-                max2 = a + b;
-            }
+        //bBH
+        for(int i = 1, bee1 = 0, bee2 = sum - arr[0] - arr[i]; i < n - 1; bee1 += arr[i], bee2 -= arr[++i]){
+            max = Math.max(max, bee1 + (bee2 * 2));
         }
-        System.out.println(max2);
 
+        //hBB
+        for(int i = n-2, bee1 = sum - arr[i] - arr[i+1], bee2 = 0; i > 0; bee2 += arr[i], bee1 -= arr[--i]){
+            max = Math.max(max, bee2 + (bee1 * 2));
+        }
 
-
+        System.out.println(max);
     }
 }
