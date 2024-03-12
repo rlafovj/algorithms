8e65bdcd949d83e9dda0cc50d762782c61dc0a51
unknown <rlafovj@gmail.com>
2024-03-12 18:01:10 +0900

    240312 1330

---
+++ baekjoon/CompareTwoNumber1330.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class CompareTwoNumber1330 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    if(a > b){
+      System.out.println(">");
+    } else if (a < b) {
+      System.out.println("<");
+    }else System.out.println("==");
+  }
+}
