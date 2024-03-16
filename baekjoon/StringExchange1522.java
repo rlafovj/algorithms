bd11c28f887883ea82fd0ca2d186bb9fd7a8a893
rlafovj <rlafovj@gmail.com>
2024-03-16 17:08:58 +0900

    240316 1522 Start

---
+++ baekjoon/StringExchange1522.java
@@ -0,0 +1,16 @@
+package baekjoon;
+//a와 b로만 이루어진 문자열이 주어질 때,  a를 모두 연속으로 만들기 위해서 필요한 교환의 회수를 최소로 하는 프로그램을 작성하시오.
+//이 문자열은 원형이기 때문에, 처음과 끝은 서로 인접해 있는 것이다.
+//예를 들어,  aabbaaabaaba 이 주어졌을 때, 2번의 교환이면 a를 모두 연속으로 만들 수 있다.
+
+import java.util.Scanner;
+
+//첫째 줄에 문자열이 주어진다. 문자열의 길이는 최대 1,000이다.
+public class StringExchange1522 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+
+
+  }
+}
