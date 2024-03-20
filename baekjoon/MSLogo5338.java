0e9177c53cce6eafd610b18cbfd606edee5f2411
rlafovj <rlafovj@gmail.com>
2024-03-20 22:21:07 +0900

    240320 5338

---
+++ baekjoon/MSLogo5338.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+public class MSLogo5338 {
+  public static void main(String[] args) {
+    System.out.println("       _.-;;-._\n" +
+            "'-..-'|   ||   |\n" +
+            "'-..-'|_.-;;-._|\n" +
+            "'-..-'|   ||   |\n" +
+            "'-..-'|_.-''-._|");
+  }
+}
