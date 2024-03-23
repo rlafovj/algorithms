959752821eff43667b251abfd7e890a77e74fab8
rlafovj <rlafovj@gmail.com>
2024-03-23 22:24:04 +0900

    240323

---
+++ baekjoon/FourBasicOperations10869.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class FourBasicOperations10869 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    System.out.println(a%b);
+  }
+}
