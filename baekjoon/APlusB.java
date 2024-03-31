ac4e13845eeeaeb91bcd0d1e8c376b0f96f2edc5
rlafovj <rlafovj@gmail.com>
2024-03-31 22:45:42 +0900

    240331

--- baekjoon/APlusB.java
+++ baekjoon/APlusB.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class APlusB {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a, b;
+        long a, b, c;
         a = sc.nextInt();
         b = sc.nextInt();
-        System.out.println(a+b);
+        c = sc.nextInt();
+        System.out.println(a+b+c);
     }
 }
